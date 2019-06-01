42d171617407f83a1c97e4ad80a29be0cfd0c51c
Alex <osdalex@gmail.com>
2019-06-01 16:05:30 +0500

    Bump up version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='confj',
-    version='0.1.2',
+    version='0.1.3',
     description='Load configs from json files',
     long_description=long_description,
     long_description_content_type='text/markdown',
